ab8b557758aa215a0e0fba412fb3a361de3c39b4
mvj23 <michaelji2021@u.northwestern.edu>
2020-09-03 18:30:40 -0500

    initial commit

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello, World!</Text>
+      <Image
+        source={require('./assets/not_found.png')}
+      />
       <StatusBar style="auto" />
     </View>
   );
