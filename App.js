925c4191f7cbe88d81eeae2cf926f0ad924570a2
mvj23 <michaelji2021@u.northwestern.edu>
2020-09-03 19:50:11 -0500

    basic format set, separate style file

--- App.js
+++ App.js
@@ -1,24 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
+import { app, header, headerText, container, navBar } from './Styles';
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { Text, View, Image, Button } from 'react-native';
 
-export default function App() {
+const Header = () => {
   return (
-    <View style={styles.container}>
-      <Text>Hello, World!</Text>
+    <View style={header}>
+      <Text style={headerText}>xkcd 404</Text>
+      <Button title="Title Text"/>
+      <Button title="Search"/>
+    </View>
+  )
+}
+
+const Comic = () => {
+  return (
+    <View style={container}>
+      <Text>Not Found</Text>
       <Image
         source={require('./assets/not_found.png')}
       />
       <StatusBar style="auto" />
     </View>
+  )
+}
+
+const NavBar = () => {
+  return (
+    <View style={navBar}>
+      <Button title="First"/>
+      <Button title="Previous"/>
+      <Button title="Random"/>
+      <Button title="Next"/>
+      <Button title="Latest"/>
+    </View>
+  )
+}
+
+const App = () => {
+  return (
+    <View style={app}>
+      <Header/>
+      <Comic/>
+      <NavBar/>
+    </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ Styles.js
@@ -0,0 +1,25 @@
+export const app = {
+    flex: 1,
+};
+export const header = {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+};
+export const headerText = {
+    color: '#fff',
+}
+export const container = {
+    flex: 6,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+};
+export const navBar = {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+};
